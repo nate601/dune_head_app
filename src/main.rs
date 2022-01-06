3d3bf90701486f1348d307544ce2a9de5578c9bb
Nathan Button <nathanebutton@gmail.com>
2022-01-06 01:17:24 -0600

    Add discord webhook functionality

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use serde_derive::Deserialize;
-use std::io;
+use rand::seq::SliceRandom;
+use serde_derive::{Deserialize, Serialize};
 use serde_xml_rs::from_str;
+use std::env;
 
 #[derive(Debug, Deserialize)]
 struct Rss {
@@ -15,10 +16,12 @@ struct Channel {
 struct Item {
     title: String,
     news_item: Vec<NewsItem>,
+    picture: String,
 }
 #[derive(Debug, Deserialize)]
 struct NewsItem {
     news_item_title: String,
+    news_item_url: String,
 }
 fn main() {
     let client = reqwest::blocking::Client::new();
@@ -32,13 +35,70 @@ fn main() {
     let rss: Rss = from_str(&res).unwrap();
     // println!("{:?}",rss);
     let items = rss.channel.item;
-    for i in items {
+    for i in &items {
         let title = htmlescape::decode_html(&i.title).expect("Unable to decode title");
-        println!("{} this, {} that.  I need some head.", title, title);
+        let meme_text = get_meme_text(&title);
+        println!("{}", meme_text);
+        // println!("{} this, {} that.  I need some head.", title, title);
         let news_title = htmlescape::decode_html(&i.news_item[0].news_item_title)
             .expect("Unable to decode news_title");
         println!("\t{}", news_title);
     }
-    let mut discard_string = String::new();
-    io::stdin().read_line(&mut discard_string).ok();
+    let mut rng = rand::thread_rng();
+    let choice = items.choose(&mut rng).expect("Unable to randomly choose");
+    send_discord_message(
+        client,
+        get_meme_text(&htmlescape::decode_html(&choice.title).expect("unable to decode title")),
+        choice.news_item[0].news_item_url.to_string(),
+        htmlescape::decode_html(&choice.news_item[0].news_item_title)
+            .expect("Unable to decode news title")
+            .to_string(),
+        choice.picture.to_string(),
+    );
+}
+#[derive(Debug, Serialize)]
+struct DiscordMessage {
+    embeds: Vec<DiscordEmbed>,
+}
+#[derive(Debug, Serialize)]
+struct DiscordEmbed {
+    title: String,
+    url: String,
+    description: String,
+    thumbnail: DiscordImage,
+}
+#[derive(Debug, Serialize)]
+struct DiscordImage {
+    url: String,
+}
+
+fn send_discord_message(
+    client: reqwest::blocking::Client,
+    message: String,
+    link: String,
+    desc: String,
+    picture: String,
+) {
+    /*
+    let mut map:HashMap<String, String> = HashMap::new();
+    map.insert("".to_string(), "".to_string());
+    map.insert("content".to_string(), message);
+    */
+    let body = DiscordMessage {
+        embeds: vec![DiscordEmbed {
+            title: message.to_string(),
+            url: link,
+            description: desc,
+            thumbnail: DiscordImage { url: picture },
+        }],
+    };
+
+    let discord_api = env::var("HEAD_API").expect("Unable to give head");
+    let _ = client.post(discord_api)
+        .json(&body)
+        .send()
+        .expect("Unable to send discord message!");
+}
+fn get_meme_text(title: &String) -> String {
+    format!("{} this, {} that.  I need some head.", title, title)
 }
