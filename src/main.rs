4fdd96c6f9985d93a561c4788eb6dd3f40fcb5af
Nathan Button <nathanebutton@gmail.com>
2022-06-03 23:24:20 -0500

    Update String.replace() usage

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ pub use crate::discord::*;
 mod rss;
 pub use crate::rss::*;
 
-fn main() {
+fn main()
+{
     let client = reqwest::blocking::Client::new();
     let res = client
         .get("https://trends.google.com/trends/trendingsearches/daily/rss?geo=US")
@@ -15,9 +16,11 @@ fn main() {
         .expect("Unable to parse response!");
     let rss_string: Rss = from_str(&res).unwrap();
     let items = rss_string.channel.item;
-    for i in &items {
+    for i in &items
+    {
         let title = htmlescape::decode_html(&i.title).expect("Unable to decode title");
         let meme_text = get_meme_text(&title);
+        println!("{}", i.approx_traffic);
         println!("{}", meme_text);
         let news_title = htmlescape::decode_html(&i.news_item[0].news_item_title)
             .expect("Unable to decode news_title");
@@ -25,7 +28,16 @@ fn main() {
     }
     let mut rng = rand::thread_rng();
     let choice = items.choose(&mut rng).expect("Unable to randomly choose");
-    let headline =  items.iter().max_by_key(|p| p.approx_traffic.replace(&[',','+'], "").parse::<i32>().unwrap()).unwrap_or(choice);
+    let headline = items
+        .iter()
+        .max_by_key(|p| {
+            p.approx_traffic
+                .replace(",", "")
+                .replace("+", "")
+                .parse::<i32>()
+                .unwrap()
+        })
+        .unwrap_or(choice);
     send_discord_message(
         client,
         get_meme_text(&htmlescape::decode_html(&headline.title).expect("unable to decode title")),
@@ -33,9 +45,10 @@ fn main() {
         htmlescape::decode_html(&headline.news_item[0].news_item_title)
             .expect("Unable to decode news title")
             .to_string(),
-            headline.picture.to_string(),
+        headline.picture.to_string(),
     );
 }
-fn get_meme_text(title: &String) -> String {
+fn get_meme_text(title: &String) -> String
+{
     format!("{} this, {} that.  I need some head.", title, title)
 }
